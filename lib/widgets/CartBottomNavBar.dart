a2be9b3cb839a78cfa96fcd313bb1c1e48a12ddd
hmadilkhan <adilsziu@outlook.com>
2023-10-07 09:08:30 +0500

    App Completed

--- lib/widgets/CartBottomNavBar.dart
+++ lib/widgets/CartBottomNavBar.dart
@@ -5,6 +5,48 @@ class CartBottomNavBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return BottomAppBar(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20,vertical: 15),
+        height: 130,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("Total",
+                  style: TextStyle(
+                  color: Color(0xFF4C53A5),
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold
+                ),),
+                Text("\$250",
+                  style: TextStyle(
+                      color: Color(0xFF4C53A5),
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold
+                  ),)
+              ],
+            ),
+            Container(
+              alignment: Alignment.center,
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFF4C53A5),
+                borderRadius: BorderRadius.circular(20)
+              ),
+              child: Text("Check Out",
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+              ),),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
